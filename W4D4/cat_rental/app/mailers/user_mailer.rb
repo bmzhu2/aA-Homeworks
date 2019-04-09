ca619bf694397bfff1c3166b43b8881655f79fcc
bmzhu2 <brian.m.zhu@gmail.com>
2019-04-09 00:20:59 -0700

    Polymorphism and concern practice

--- W4D4/cat_rental/app/mailers/user_mailer.rb
+++ W4D4/cat_rental/app/mailers/user_mailer.rb
@@ -4,6 +4,6 @@ class UserMailer < ApplicationMailer
     def welcome_email(user)
         @user = user
         @url  = 'http://zombo.com/'
-        mail(to: 'thisis@zombo.com', subject: 'Welcome to zombo com')
+        mail(to: user.username, subject: 'Welcome to zombo com')
     end
 end
